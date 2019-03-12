8f7463233b77827121b847630b9cf94e23ad8832
Andre Rocha <andrefmrocha@live.com.pt>
2019-03-12 11:00:26 +0000

    Added gradle files

---
+++ src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -0,0 +1,59 @@
+package com.aor.numbers;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ListAggregatorTest {
+
+    List<Integer> list;
+
+    @Before
+    private void prepareList(){
+        this.list.clear();
+        list.add(1);
+        list.add(2);
+        list.add(4);
+        list.add(2);
+        list.add(5);
+    }
+    @Test
+    public void sum() {
+        ListAggregator aggregator = new ListAggregator(list);
+
+        int sum = aggregator.sum();
+
+        assertEquals(14, sum);
+    }
+
+    @Test
+    public void max() {
+        ListAggregator aggregator = new ListAggregator(list);
+
+        int max = aggregator.max();
+
+        assertEquals(5, max);
+    }
+
+    @Test
+    public void min() {
+        ListAggregator aggregator = new ListAggregator(list);
+
+        int min = aggregator.min();
+
+        assertEquals(1, min);
+    }
+
+    @Test
+    public void distinct() {
+        ListAggregator aggregator = new ListAggregator(list);
+
+        int distinct = aggregator.distinct();
+
+        assertEquals(4, distinct);
+    }
+}
